bfa05175dae79b5dd708808ca2631f638e433235
KhlebnikovMikhaill <khleb-mikh@mail.ru>
2022-06-23 16:17:17 +0300

    Исправление ошибок в main

--- ChartExamples_3.py
+++ ChartExamples_3.py
@@ -10,17 +10,17 @@ import chart4
 # Главная функция
 def main():    
     # График синуса matplotlib
-    chart1.plot_chart    
+    chart1.plot_chart()    
     # График нормального распределения
-    chart2.plot_chart    
+    chart2.plot_chart()   
     # График FillBetween
-    chart3.plot_chart    
+    chart3.plot_chart()    
     # Нормальное распределение - 3 графика
-    chart2.plot_chart2    
+    chart2.plot_chart2()    
     # Гистограмма SeaBorn
-    chart4.plot_chart    
+    chart4.plot_chart() 
     # Сдвоенная гистограмма SeaBorn
-    chart4.plot_chart2    
+    chart4.plot_chart2()   
     return 0
 
 if __name__ == '__main__':
