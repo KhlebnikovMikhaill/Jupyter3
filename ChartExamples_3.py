b6796e10816fc6c81a5029ad948970e4b47d2890
KhlebnikovMikhaill <khleb-mikh@mail.ru>
2022-06-23 14:51:32 +0300

    Переименование файла

--- ChartExamples_3.py
+++ ChartExamples_3.py
@@ -1,5 +1,4 @@
-# Программа построения графиков
- 
+# Программа построения графиков 
 import tkinter as tk
 
 # Импорт внешних файлов
@@ -9,27 +8,19 @@ import chart3
 import chart4
  
 # Главная функция
-
-def main():
-    
+def main():    
     # График синуса matplotlib
-    chart1.plot_chart
-    
+    chart1.plot_chart    
     # График нормального распределения
-    chart2.plot_chart
-    
+    chart2.plot_chart    
     # График FillBetween
-    chart3.plot_chart
-    
+    chart3.plot_chart    
     # Нормальное распределение - 3 графика
-    chart2.plot_chart2
-    
+    chart2.plot_chart2    
     # Гистограмма SeaBorn
-    chart4.plot_chart
-    
+    chart4.plot_chart    
     # Сдвоенная гистограмма SeaBorn
-    chart4.plot_chart2
-    
+    chart4.plot_chart2    
     return 0
 
 if __name__ == '__main__':
